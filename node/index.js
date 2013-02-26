60b98e79bfd709a19adb2e4a11fd2adea9437bd6
Sinewyk <Sinewyk@gmail.com>
2013-02-26 23:45:12 +0100

    small refactor change for the Array extension edit gitignore to ignore log generated by node.js

--- node/index.js
+++ node/index.js
@@ -6,19 +6,8 @@
 |_| \_|\___/ \__,_|\___|
 **/
 
-/**
-Array extension
-**/
-Array.prototype.remove = function() {
-    var what, a = arguments, L = a.length, ax;
-    while (L && this.length) {
-        what = a[--L];
-        while ((ax = this.indexOf(what)) !== -1) {
-            this.splice(ax, 1);
-        }
-    }
-    return this;
-};
+//Extend the prototype of Array
+require("./models/Array.js");
 
 var config = require("./config")
 , server = require("./server")
@@ -34,12 +23,12 @@ global.accessLog = accessLog;
 global.errorLog = errorLog;
 
 //If server launched in debug mode, console.log working, else not
-if(process.argv.indexOf("-debug") == -1) {
+if(process.argv.indexOf("-debug") === -1) {
     console.log("Debug mode : OFF");
     console.log = function() {};
 } else console.log("Debug mode : ON");
 
-console.log("Started app with config :",config);
+console.log("Started app with config :\n",config);
 
 global.config = config;
 
---
+++ node/models/Array.js
@@ -0,0 +1,13 @@
+/**
+Simple array extension to easily remove something(s)
+**/
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
